1869bfbc2b21acf9869d10152024a27cb5a61f26
yasfu <fumiaki2381@gmail.com>
2021-05-13 14:30:13 +0900

    バリデーション追加・エラーメッセージ表記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
 
   validates :nick_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+
+  has_many :products
+  has_many :orders
 end
