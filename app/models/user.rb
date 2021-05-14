403c023e648e99a85276970defc610d246cf36a5
yasfu <fumiaki2381@gmail.com>
2021-05-15 00:22:03 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,19 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
 
-  validates :nick_name, presence: true
-  validates :birthday, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
+  with_options presence: true do
+    validates :nick_name
+    validates :birthday
   end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :last_name
+    validates :first_name
+  end
+
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width katakana characters' } do
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
   has_many :products
