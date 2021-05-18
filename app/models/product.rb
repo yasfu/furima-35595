bbaa65c5f34db810c3deb1320eec117d15d58d0e
yasfu <fumiaki2381@gmail.com>
2021-05-18 18:13:05 +0900

    テストコード

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :shipping_charge, :category, :state, :day_to_ship, :prefecture
+  belongs_to :shipping_charge 
+  belongs_to :category
+  belongs_to :state
+  belongs_to :day_to_ship
+  belongs_to :prefecture
   has_one_attached :image
+  belongs_to :user
 
   with_options presence: true do
     validates :image
@@ -14,8 +19,8 @@ class Product < ApplicationRecord
     validates :day_to_ship_id
     validates :price
   end
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-  validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  validates :price, numericality: {only_integer: true, message: "is invalid. Input half-width characters" }
   with_options numericality: {other_than: 1, message: "can't be blank"} do
     validates :category_id
     validates :state_id
