833eba799974d3c3d10f117dc7233ed07e307853
yasfu <fumiaki2381@gmail.com>
2021-05-19 11:48:41 +0900

    商品一覧機能の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @products = Product.all
+    @products = Product.all.order('created_at DESC')
   end
 
   def new
