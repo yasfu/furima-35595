131bb077350b3b45d1cba2dbaff1511a67bd081d
yasfu <fumiaki2381@gmail.com>
2021-05-19 13:56:03 +0900

    アクションの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,9 @@ class ProductsController < ApplicationController
       render :new
     end
   end
-
+  def show
+    @product = Product.find(params[:id])
+  end
   private
 
   def product_params
