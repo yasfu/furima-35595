fab955c4e476f6ecd28c55b0e7cb0cbf8423fea1
yasfu <fumiaki2381@gmail.com>
2021-05-19 18:19:34 +0900

    商品編集昨日の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_product, only: [:edit, :show, :update]
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -19,17 +20,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     redirect_to root_path unless current_user.id == @product.user_id
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
+      redirect_to root_path unless current_user.id == @product.user_id
       redirect_to product_path
     else
       render :edit
@@ -41,5 +40,9 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:image, :name, :description, :category_id, :state_id, :shipping_charge_id, :prefecture_id,
                                     :day_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
